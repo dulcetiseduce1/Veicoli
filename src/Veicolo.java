fcf6acd76906b0255fa970902b15639fca27a43f
dulcetiseduce <swinnaiscool@gmail.com>
2020-03-24 19:36:38 +0100

    Initial commit

---
+++ src/Veicolo.java
@@ -0,0 +1,13 @@
+public class Veicolo {
+   private int numeroMatricola;
+   private int nomeProprietario;
+
+    public Void assegnaProprietario(String nome)
+
+    public String dammiProprietario()
+
+    public void assegnaMatricola(int matricola)
+
+    public int dammiMatricola()
+
+}
---
+++ src/provaVeicolo.java
@@ -0,0 +1,39 @@
+//test classe Veicolo Auto Moto Camion
+
+public class provaVeicolo {
+    public static void main(String[] arg) {
+
+
+        auto a = new auto();
+        a.assegnaProprietario("Luca");
+        a.assegnaMatricola(1234);
+        a.assegnaTipo("Sport");
+
+
+        camion c = new camion();
+        c.assegnaProprietario("Anna");
+        c.assegnaMatricola(34231);
+        c.assegnaAssi(3);
+
+
+        moto m = new moto();
+        m.assegnaProprietario("Valentino");
+        m.assegnaMatricola(8765);
+        m.assegnaNumPosti(2);
+
+        System.out.println("Auto di: " + a.dammiProprietario() + "tipo dell'auto: " + a.dammiTipo());
+
+        System.out.println("Camion di: " + c.dammiProprietario() + "numero Matricola: " + c.dammiMatricola() + "numero assi: " + c.dammiAssi());
+
+        System.out.println("Moto di: " + m.dammiProprietario() + "numero Matricola: " + m.dammiMatricola() + "numero Posti: ") + m.dammiNumPosti());
+
+        veicolo v = new veciolo();
+        v = m;
+
+        if (v instanceof moto)
+            System.out.println("il veicolo è una moto");
+
+        moto m2 = (moto)v;
+        System.out.println(m2.dammiNumPosti()))
+    }
+}
